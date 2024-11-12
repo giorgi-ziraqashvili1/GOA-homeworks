ecd543c57fc111a8aefb9eae3bd5d8dfe82d4057
giorgi-ziraqashvili1 <g.ziraqashvili1@icloud.com>
2024-11-12 11:52:07 +0400

    commit

--- Level 17/Homework/task1.py
+++ Level 17/Homework/task1.py
@@ -2,6 +2,8 @@
 
 cars=[ "bmw", "mercedes", "audi","toyta", "Konissegge"]
 
+print(cars[0])
+
 print(cars[1])
 
 print(cars[2])
@@ -9,8 +11,6 @@ print(cars[2])
 print(cars[3])
 
 print(cars[4])
-
-print(cars[0])
 
 """"2)შექმენით სია შემდგარი 10 ელემენტისგან და გამოიტანეთ თითოეული ელემენტი for ციკლით / range() ფუნქციითაც და პირდაპირ ცვლადის გადაცემითაც"""
 
