a6ed700949692825363c55e13e771e7a6dc84dfc
giorgi-ziraqashvili1 <g.ziraqashvili1@icloud.com>
2024-12-09 21:33:10 +0400

    commit

---
+++ Level 27/Homework/task.py
@@ -0,0 +1,58 @@
+# 1)შექმენით ფუნქცია რომელსაც არგუმენტად გადაეცემა ორი რიცხვი შემდეგ კი გამოიტანეთ ამ რიცხვების ჯამი.
+
+number=[5,10]
+
+sum = 0
+
+
+def list():
+    for i in number:
+        sum + i
+        print(sum)
+
+
+list()
+        
+# 2)შექმენით ფუნქცია რომელსაც გადაეცემა არგუმენტად რიცხვი შემდეგ კი მან უნდა დაგვიბრუნოს ლუწია ეს რიცხვი თუ კენტი.
+num = 5
+
+def Giorgii():
+    if num % 2 == 0:
+        print("luwia")
+    else:
+        print("Kentia")
+
+Giorgii()
+
+
+
+
+
+# 3)შექმენით ფუნქცია რომელსაც არგუმენტად გადაეცემა რიცხვი შემდეგ კი მან უნდა დაგვიბრუნოს ეს რიცხვი დადებითია თუ უარყოფითი.
+
+num2= 20
+def ziraka():
+    if num2 > 0:
+        print("Dadebitia")
+    elif num2 < 0:
+        print("Uaryofitia")
+    else:
+        print("Ricxvi Nulia")
+
+ziraka()
+
+
+# 4)შექმენით ფუნქცია, რომელსაც არგუმენტად გადაეცემა სახელი და დაბეჭდავს მისთვის მიესალმებას (მაგალითად: "Hello Giorgi"). გამოძახეთ ეს ფუნქცია 2-ჯერ და გადაეცით სხვადასხვა სახელი.
+
+def name(name):
+    print(name, "Hello")
+
+name("Gio")
+
+# 5)შექმენით ფუნქცია, რომელიც იღებს ორ სტრინგს და მოახდინეთ კონკატენაცია.
+
+def concatination(Gio,Ziraka):
+    for i in concatination(Gio,Ziraka):
+        print(Gio + Ziraka)
+
+concatination()
