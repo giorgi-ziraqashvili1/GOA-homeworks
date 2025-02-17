2634178b1f7f75fd4a7fbdf2390440ee47884ed1
giorgi-ziraqashvili1 <g.ziraqashvili1@icloud.com>
2025-02-17 20:53:19 +0400

    commit

---
+++ Level 66/Classwork/script.js
@@ -0,0 +1,13 @@
+var num = 10
+var num1 = 15
+var num2 = 20
+var num3 = 5
+
+console.log(num & num1 == num2 & num3)
+
+console.log(num |num1 > num2 & num3)
+
+console.log(num & num1 < num2 | num3)
+
+console.log(num | num1 == num2 | num3)
+
